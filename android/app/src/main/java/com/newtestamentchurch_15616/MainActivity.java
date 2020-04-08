18abc9dd9dcedfb61b10d617d4bf4371ecf36d25
Crowdbotics <team@crowdbotics.com>
2020-04-08 22:09:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/newtestamentchurch_15616/MainActivity.java
@@ -0,0 +1,15 @@
+package com.newtestamentchurch_15616;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "newtestamentchurch_15616";
+    }
+}
